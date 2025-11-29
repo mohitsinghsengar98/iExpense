8d2a899cb0553e621aa0983c9a2d8cce440936ae
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-29 22:35:28 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mohit Sengar on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
