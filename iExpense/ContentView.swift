cd1598ee4b215e3fb2c1e3570801fe665cc72c2e
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-29 22:52:48 +0530

    @observable use.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,13 +7,22 @@
 
 import SwiftUI
 
+@Observable
+class User{
+    var firstName = "Mohit"
+    var lastName = "Sengar"
+}
+// Use the class to store because when we use struct it will work but whenever the values update it will create the multiple copies of that struct on every change.
+
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("My Name is \(user.firstName) \(user.lastName)")
+            
+            TextField("Enter your first name", text: $user.firstName) // here note that when the whole class is observable then all the variables will be used as the observable and also marked as state then they work as observable objects to store and work on.
+            TextField("Enter your last name",text: $user.lastName)
         }
         .padding()
     }
